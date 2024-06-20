86910c347b1359b0543a8ee8a424879a2ce91b4e
amauryguerin <amauryguerin44@gmail.com>
2024-06-20 22:58:26 +1000

    update film page, dynamic height video

--- pages/projects/[slug]/film.vue
+++ pages/projects/[slug]/film.vue
@@ -1,5 +1,5 @@
 <template>
-    <video width="100%" height="auto" autoplay muted controls>
+    <video width="100%" :style="{ height: availableHeight }" autoplay muted controls>
         <source :src="projectSingleData.project.projectVideo.url" />
         Your browser does not support the video tag.
     </video>
@@ -10,6 +10,18 @@
 <script setup>
 import projectBySlug from '@/api/queries/projectBySlug'
 import siteFooter from '@/api/queries/siteFooter'
+import { onMounted, ref, nextTick } from 'vue';
+import { useSiteLayoutStore } from '@/stores/siteLayout.js';
+
+const siteLayoutStore = useSiteLayoutStore();
+const availableHeight = ref(0);
+onMounted(async () => {
+    await nextTick();
+    const headerHeight = siteLayoutStore.headerHeight;
+    const footerHeight = siteLayoutStore.footerHeight;
+    availableHeight.value = `calc(100vh - ${headerHeight}px - ${footerHeight}px)`;
+});
+
 const route = useRoute()
 const { data: projectSingleData, pending: projectSinglePending, error: projectSingleError } = await useLazyAsyncQuery(projectBySlug, { slug: route.params.slug })
 const { data: siteFooterData, pending: siteFooterPending, error: siteFooterError } = await useLazyAsyncQuery(siteFooter);
